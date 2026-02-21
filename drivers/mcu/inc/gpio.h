bfab87ab3aafdc64d4426a68fee0a86d3108ee8a
Camryn Ruiz <camryn.ruiz@stryker.com>
2026-02-20 22:08:27 -0500

    Started GPIO Driver

---
+++ drivers/mcu/inc/gpio.h
@@ -0,0 +1,59 @@
+#include <stdint.h>
+
+#ifndef _GPIO_H
+#define _GPIO_H
+
+// GPIO Peripheral Register Offsets:
+typedef struct {
+    volatile uint32_t MODER;    // 0x00
+    volatile uint32_t OTYPER;   // 0x04
+    volatile uint32_t OSPEEDR;  // 0x08
+    volatile uint32_t PUPDR;    // 0x0C
+    volatile uint32_t IDR;      // 0x10
+    volatile uint32_t ODR;      // 0x14
+    volatile uint32_t BSRR;     // 0x18
+    volatile uint32_t LCKR;     // 0x1C
+    volatile uint32_t AFRL;     // 0x20
+    volatile uint32_t AFRH;     // 0x24
+} GPIO_TypeDef;
+
+// GPIO Peripheral Offsets:
+#define GPIOA_BASE_OFFSET (0x0000U)
+#define GPIOB_BASE_OFFSET (0x0400U)
+#define GPIOC_BASE_OFFSET (0x0800U)
+#define GPIOD_BASE_OFFSET (0x0C00U)
+#define GPIOE_BASE_OFFSET (0x1000U)
+#define GPIOF_BASE_OFFSET (0x1400U)
+#define GPIOG_BASE_OFFSET (0x1800U)
+#define GPIOH_BASE_OFFSET (0x1C00U)
+#define GPIOI_BASE_OFFSET (0x2000U)
+#define GPIOJ_BASE_OFFSET (0x2400U)
+#define GPIOK_BASE_OFFSET (0x2800U)
+
+// GPIO Peripheral Base Addresses:
+#define GPIOA_BASE_ADDR (AHB1_BASE_ADDR + GPIOA_BASE_OFFSET)
+#define GPIOB_BASE_ADDR (AHB1_BASE_ADDR + GPIOB_BASE_OFFSET)
+#define GPIOC_BASE_ADDR (AHB1_BASE_ADDR + GPIOC_BASE_OFFSET)
+#define GPIOD_BASE_ADDR (AHB1_BASE_ADDR + GPIOD_BASE_OFFSET)
+#define GPIOE_BASE_ADDR (AHB1_BASE_ADDR + GPIOE_BASE_OFFSET)
+#define GPIOF_BASE_ADDR (AHB1_BASE_ADDR + GPIOF_BASE_OFFSET)
+#define GPIOG_BASE_ADDR (AHB1_BASE_ADDR + GPIOG_BASE_OFFSET)
+#define GPIOH_BASE_ADDR (AHB1_BASE_ADDR + GPIOH_BASE_OFFSET)
+#define GPIOI_BASE_ADDR (AHB1_BASE_ADDR + GPIOI_BASE_OFFSET)
+#define GPIOJ_BASE_ADDR (AHB1_BASE_ADDR + GPIOJ_BASE_OFFSET)
+#define GPIOK_BASE_ADDR (AHB1_BASE_ADDR + GPIOK_BASE_OFFSET)
+
+// GPIO Peripheral Pointers:
+#define GPIOA ((GPIO_TypeDef*) GPIOA_BASE_ADDR)
+#define GPIOB ((GPIO_TypeDef*) GPIOB_BASE_ADDR)
+#define GPIOC ((GPIO_TypeDef*) GPIOC_BASE_ADDR)
+#define GPIOD ((GPIO_TypeDef*) GPIOD_BASE_ADDR)
+#define GPIOE ((GPIO_TypeDef*) GPIOE_BASE_ADDR)
+#define GPIOF ((GPIO_TypeDef*) GPIOF_BASE_ADDR)
+#define GPIOG ((GPIO_TypeDef*) GPIOG_BASE_ADDR)
+#define GPIOH ((GPIO_TypeDef*) GPIOH_BASE_ADDR)
+#define GPIOI ((GPIO_TypeDef*) GPIOI_BASE_ADDR)
+#define GPIOJ ((GPIO_TypeDef*) GPIOJ_BASE_ADDR)
+#define GPIOK ((GPIO_TypeDef*) GPIOK_BASE_ADDR)
+
+#endif // _GPIO_H
--- makefile
+++ makefile
@@ -2,17 +2,20 @@ COMPILER = arm-none-eabi-gcc
 APPLICATION_CODE = application/*.c
 LINKER_SCRIPT = linker/stm32f429_linker.ld
 STARTUP_SCRIPT = startup/stm32f429_startup.c
-OUT_DIR = output
+OUT_DIR = build
 ELF_FILE = $(OUT_DIR)/blink.elf
 OPT ?= -O0
 CFLAGS  = -mcpu=cortex-m4 -mthumb -nostdlib $(OPT)
 LDFLAGS = -T $(LINKER_SCRIPT) -Wl,--build-id=none
+INC_DIRS = -I drivers/mcu/inc
 
 .PHONY: build flash clean
 
 build:
+	@mkdir -p $(OUT_DIR)
+	@rm -f $(ELF_FILE)
 	@echo "Compiling application..."
-	$(COMPILER) $(APPLICATION_CODE) $(STARTUP_SCRIPT) $(CFLAGS) $(LDFLAGS) -o $(ELF_FILE)
+	$(COMPILER) $(APPLICATION_CODE) $(STARTUP_SCRIPT) $(CFLAGS) $(LDFLAGS) -o $(ELF_FILE) $(INC_DIRS)
 
 flash:
 	@echo "======================================"

3621fc01970917c13e1560d5d84139e530a4a48a
Camryn Ruiz <camryn.ruiz@stryker.com>
2026-02-23 11:49:26 -0500

    Added RCC_TypeDef

--- application/main.c
+++ application/main.c
@@ -4,25 +4,35 @@
 #include "gpio.h"
 
 // STM32F429 Eval Board on Board LEDs:
+#define USER_BUTTON_PIN 0U
 #define GREEN_LED_PIN 13U
 #define RED_LED_PIN 14U
 #define GREEN_LED_MASK (1U << GREEN_LED_PIN)
 #define RED_LED_MASK (1U << RED_LED_PIN)
 
 void main(void) {
-    *RCC_AHB1ENR |= (1 << RCC_AHB1ENR_GPIOGEN); // Turn on RCC for GPIOG Peripheral
+    RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN); // Turn on RCC for GPIOA Peripheral
+    RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOGEN); // Turn on RCC for GPIOG Peripheral
 
     //GPIOG->MODER &= ~((3 << 26) | (3 << 28)); // Clear Bits for Pins 13 & 14
     //GPIOG->MODER |= (1 << 26) | (1 << 28); // Set MODER13=01 & MODER14=01
+    gpio_set_mode(GPIOA, USER_BUTTON_PIN, GPIO_MODE_INPUT); // Set PA0 as Input (User Button)
     gpio_set_mode(GPIOG, GREEN_LED_PIN, GPIO_MODE_OUTPUT);
     gpio_set_mode(GPIOG, RED_LED_PIN, GPIO_MODE_OUTPUT);
     gpio_set_pin(GPIOG, GREEN_LED_PIN);
 
     while (1) {
         //GPIOG->BSRR = RED_LED_MASK | (GREEN_LED_MASK << 16); // LEDs <R|G> : <ON|OFF>
-        gpio_toggle_pin(GPIOG, GREEN_LED_PIN);
-        gpio_toggle_pin(GPIOG, RED_LED_PIN);
-        for (volatile uint32_t i = 0; i < 1000000; i++); // Delay
+        //gpio_toggle_pin(GPIOG, GREEN_LED_PIN);
+        //gpio_toggle_pin(GPIOG, RED_LED_PIN);
+        //for (volatile uint32_t i = 0; i < 1000000; i++); // Delay
+        if (gpio_read_pin(GPIOA, USER_BUTTON_PIN)) {
+            gpio_set_pin(GPIOG, RED_LED_PIN);
+            gpio_clear_pin(GPIOG, GREEN_LED_PIN);
+        } else {
+            gpio_clear_pin(GPIOG, RED_LED_PIN);
+            gpio_set_pin(GPIOG, GREEN_LED_PIN);
+        }
         //GPIOG->BSRR = (RED_LED_MASK << 16) | GREEN_LED_MASK; // LEDs <R|G> : <OFF|ON>
         //for (volatile uint32_t i = 0; i < 1000000; i++); // Delayl
     }
--- drivers/mcu/inc/rcc.h
+++ drivers/mcu/inc/rcc.h
@@ -2,7 +2,43 @@
 #include "stm32f429.h"
 
 // RCC Peripheral:
+typedef struct {
+    volatile uint32_t CR;
+    volatile uint32_t PLLCFGR;
+    volatile uint32_t CFGR;
+    volatile uint32_t CIR;
+    volatile uint32_t AHB1RSTR;
+    volatile uint32_t AHB2RSTR;
+    volatile uint32_t AHB3RSTR;
+    volatile uint32_t RESERVED0; // Reserved: 0x1C
+    volatile uint32_t APB1RSTR;
+    volatile uint32_t APB2RSTR;
+    volatile uint32_t RESERVED1[2]; // Reserved: 0x28-0x2C
+    volatile uint32_t AHB1ENR;
+    volatile uint32_t AHB2ENR;
+    volatile uint32_t AHB3ENR;
+    volatile uint32_t RESERVED2; // Reserved: 0x3C
+    volatile uint32_t APB1ENR;
+    volatile uint32_t APB2ENR;
+    volatile uint32_t RESERVED3[2]; // Reserved: 0x48-0x4C
+    volatile uint32_t AHB1LPENR;
+    volatile uint32_t AHB2LPENR;
+    volatile uint32_t AHB3LPENR;
+    volatile uint32_t RESERVED4; // Reserved: 0x5C
+    volatile uint32_t APB1LPENR;
+    volatile uint32_t APB2LPENR;
+    volatile uint32_t RESERVED5[2]; // Reserved: 0x68-0x6C
+    volatile uint32_t BDCR;
+    volatile uint32_t CSR;
+    volatile uint32_t RESERVED6[2]; // Reserved: 0x78-0x7C
+    volatile uint32_t SSCGR;
+    volatile uint32_t PLLI2SCFGR;
+    volatile uint32_t PLLSAICFGR;
+    volatile uint32_t DCKCFGR;
+} RCC_TypeDef;
+
 #define RCC_BASE (AHB1_BASE_ADDR + 0x3800)
-#define RCC_AHB1ENR_OFFSET (0x30U)
+#define RCC ((RCC_TypeDef*) RCC_BASE)
+
+#define RCC_AHB1ENR_GPIOAEN (0x00U)
 #define RCC_AHB1ENR_GPIOGEN (0x06U)
-#define RCC_AHB1ENR ((volatile uint32_t*) (RCC_BASE + RCC_AHB1ENR_OFFSET))
